21e7d8128c082f4c2641e9df8a4a10864f8aacde
ifeanyisunday <sundayjnr10@gmail.com>
2024-12-06 09:49:04 +0100

    BankEasyFrontend

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -28,9 +28,9 @@ export const Logout = () => {
 
   return (
     <>
-        {/* <form onSubmit={submitRequest}>
+        <form>
             <button type="submit"className="size-52 w-70 py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">Logout</button>
-        </form> */}
+        </form>
     </>
   )
 }
